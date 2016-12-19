59452167bcec5e3de84c17041a8b1b1410a8befd
Dmitry Skripunov <dskripunov@gmail.com>
2016-12-19 11:21:41 +0300

    mouse move and click

--- wascript-monitor.js
+++ wascript-monitor.js
@@ -6,49 +6,77 @@ function WascriptMonitor(options){
     
     this.canvas = undefined;
     this._ctx = undefined;
+    this.X = undefined;
+    this.Y = undefined;
     
     this._init = function(options){
         document.addEventListener("DOMContentLoaded", _self._ready);
     }
     
     this._ready = function(){
-        /*_self.canvas = document.createElement('canvas');
-        
-        _self.canvas.setAttribute('id', _self.CANVAS_ID);
         var body = document.querySelector('body');
-        _self.canvas.style.width      = body.offsetWidth + 'px';
-        _self.canvas.style.height     = body.offsetHeight + 'px';
-        _self.canvas.style.position   = 'absolute';
+        
+        _self.canvas = document.createElement('canvas');
+        _self.canvas.setAttribute('id', _self.CANVAS_ID);
+        _self.canvas.setAttribute('width', body.offsetWidth);
+        _self.canvas.setAttribute('height', body.offsetHeight);
+        _self.canvas.style.position   = 'fixed';
         _self.canvas.style.top        = '0';
         _self.canvas.style.left       = '0';
-        //_self.canvas.style.display    = 'none';*/
+        _self.canvas.style.display    = 'none';
         
+        body.appendChild(_self.canvas); 
+          
+        _self.ctx = _self.canvas.getContext('2d'); 
+        _self.ctx.globalAlpha = 0.4;
+        _self.ctx.fillStyle = "#ffffff";
+        _self.ctx.fillRect(0, 0, _self.canvas.width, _self.canvas.height);
+        _self.ctx.globalAlpha = 1;
         
         document.addEventListener('mousemove', _self._onMouseMove);
- 
-        
-       // body.appendChild(_self.canvas);
-        
-        var c = document.querySelector('#'+_self.CANVAS_ID);
-        var ctx   = c.getContext('2d'); 
-        
-        ctx.fillStyle = "#ff00ff";
- 
-        //ctx.fillRect(0, 0, _self.canvas.width, _self.canvas.height);
-
-         ctx.moveTo(0, 0);
-        ctx.beginPath();
-        ctx.lineTo(300, 150);
-        ctx.strokeStyle = "black";
-        ctx.stroke();
-        
+        document.addEventListener('click', _self._onMouseClick);
+        document.addEventListener('keyup', _self._onShowCanvas);
     }
     
     this._onMouseMove = function(evt){
         var x = evt.clientX;
         var y = evt.clientY;
         
+        if(_self.X === undefined){
+            _self.X = window.event.clientX;
+            _self.Y = window.event.clientY;
+        }
         
+        _self.ctx.beginPath();
+        _self.ctx.moveTo(_self.X, _self.Y);
+        _self.ctx.lineTo(x, y);
+        _self.ctx.strokeStyle = "black";
+        _self.ctx.stroke();
+        
+        _self.X = x;
+        _self.Y = y;
+    }
+    
+    this._onMouseClick = function(evt){
+        var x = evt.clientX;
+        var y = evt.clientY;
+        
+        _self.ctx.globalAlpha = 0.6;
+        _self.ctx.beginPath();
+        _self.ctx.arc(x, y, 20, 0, 2 * Math.PI, false);
+        _self.ctx.fillStyle = '#ff0000';
+        _self.ctx.fill();
+        _self.ctx.globalAlpha = 1;
+    }
+    
+    this._onShowCanvas = function(evt){
+       if(evt.keyCode === 48 && evt.ctrlKey){
+           if(_self.canvas.style.display === 'none'){
+               _self.canvas.style.display = 'block';
+           }else{
+               _self.canvas.style.display = 'none';
+           }
+       } 
     }
     
     this._init(options);
